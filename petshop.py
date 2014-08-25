d1520835491fad2920b166086ba64c1f670c05e8
Gary <garyboland@gmail.com>
2014-08-25 14:17:31 +0100

    added Enqueue & Dequeue

--- petshop.py
+++ petshop.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 class Node(object):
@@ -11,24 +12,46 @@ class Linkedlist(object):
     self.start = None
     self.end = None
 
-  def Addnode(self, Node):
+  def Enqueue(self, Node):
     if self.start == None:
       self.start = Node
     else:
       self.end.next = Node
     self.end = Node
 
-  def loop(self):
+  def Dequeue_any(self):
+    animal = self.start
+    self.start = self.start.next
+    return animal
+
+  def Loop(self):
     n = self.start
     while n:
       print n.animal, n.age
       n = n.next
 
-l = Linkedlist()
-l.Addnode(Node('cat'))
-l.Addnode(Node('dog'))
-l.Addnode(Node('lion'))
-l.Addnode(Node('tiger'))
-l.loop()
+def OlddestAnimal(catlist, doglist):
+  if catlist.start.age < doglist.start.age:
+    result = catlist.Dequeue_any()
+    return result.animal
+  elif catlist.start.age > doglist.start.age:
+    result = doglist.Dequeue_any()
+    return result.animal
+  else:
+    all_animals = [catlist, doglist]
+    random_animal = random.choice(all_animals)
+    result = random_animal.Dequeue_any()
+    return result.animal
+
+catlist = Linkedlist()
+doglist = Linkedlist()
+catlist.Enqueue(Node('cat1'))
+doglist.Enqueue(Node('dog1'))
+doglist.Enqueue(Node('dog2'))
+catlist.Enqueue(Node('cat2'))
+catlist.Loop() 
+doglist.Loop() 
+print OlddestAnimal(catlist, doglist)
+
 
 
