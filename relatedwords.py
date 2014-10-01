7258ad9134c93d148eff485d459feca7e88d753a
Gary Boland <garyboland@gmail.com>
2014-10-01 21:39:24 +0100

    no traversal algo yet, still need to work on pattern matching

--- relatedwords.py
+++ relatedwords.py
@@ -8,17 +8,26 @@ def relatedwords(a):
       allwords.append(line.strip())
 
   result = defaultdict(list)
-  for i in allwords[:10]:  # limit to first 10 words as this is cubic 
-    for j in allwords:
-      if len(j) != len(i): continue
-      if i == j: continue
-      for k, char in enumerate(j):
-        pat = j[:k] + '.' + j[k+1:]
-        if re.search(pat, i):
-          result[i].append(j)
+  for word in allwords[:100]:  # limit to first 10 words as this is cubic 
+    for i, char in enumerate(word):
+      pat = word[:i] + '.' + word[i+1:]
+      result[pat].append(word)
+  for word in allwords[:100]:  # limit to first 10 words as this is cubic 
+    if 
   return result
 
+# Naive way
+
+def distance(word1, word2):
+    difference = 0
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            difference += 1
+    return difference
+
+def getchildren(word, wordlist):
+    return [ w for w in wordlist if distance(word, w) == 1 ]
 
 # Sanity check
-print relatedwords('words.txt')
+print relatedwords('/usr/share/dict/words')
 
