165c09b1df4235a8348a071b6f3390daa3db3714
Gary Boland <garyboland@gmail.com>
2016-03-02 23:04:22 +0000

    musicwalk that finds duplicate music files

--- musicwalk.py
+++ musicwalk.py
@@ -1,14 +1,29 @@
 import os
+import sys
 from timerdecorator import mytimer
+from collections import defaultdict
 
-@mytimer
+#@mytimer
 def walker(p):
+  if os.path.isdir(p) == False:
+    raise
   c = 0
+  filestore = defaultdict(list)
   for root, dirs, files in os.walk(p):
-    path = root.split('/')
     for fil in files:
-      c += 1
-  return c
+      #if os.path.isfile(fil):
+        #if not fil.startswith('.'):
+      filepath = root + '/' + fil
+      filestore[fil].append((filepath, os.stat(filepath).st_size))
+  print sys.getsizeof(filestore), len(filestore)
+  return filestore
+
+def dups(target):
+  filestore = walker(target)
+  result = [x for x in filestore.values() if len(x) > 1]
+  return result
+
 
 if __name__ == '__main__':
-  print walker('/mnt/pi/')
+  res = dups(sys.argv[1])
+  print res, '\n', len(res)
