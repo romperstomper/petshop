50679a0e3bf58741d5b1a7b39e8a21c699c1f853
gary boland <garyboland@gmail.com>
2016-04-19 20:54:09 +0100

    working number decorator

--- numdecorator.py
+++ numdecorator.py
@@ -4,19 +4,21 @@ from functools import wraps
 
 def mydec(func):
   def wrapped_func(*args):
-    for i, j in enumerate(args[0]):
-      print i, j
+    myargs = list(args[0])
+    for i, j in enumerate(myargs):
+      myargs[i] =  '+91' + str(j[-10:])
+    return func(myargs)
   return wrapped_func
 
 @mydec
 def sortnums(n):
-  pass
+  n.sort()
+  for elem in n:
+    print elem
 
 
 if __name__ == '__main__':
   data = sys.stdin.read()
   nums = [x for x in data.split('\n') if len(x) >1]
   sortnums(nums)
-  for num in nums:
-    print num
 
