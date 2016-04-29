e61285c2068f5cc7d66ad453ae1671556fa5a5a2
Gary Boland <garyboland@gmail.com>
2016-04-29 22:43:04 +0100

    background_changer.py

--- background_changer.py
+++ background_changer.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python
 import os
+from subprocess import call
+import time
+import random
+
+files = os.listdir('/home/gary/xkcd') 
+random.shuffle(files)
+for i in files:
+  phile = '/home/gary/xkcd/' + i
+  call(['feh', '--bg-center', phile])
+  time.sleep(30)
